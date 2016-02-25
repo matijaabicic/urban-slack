6f75902fb70c93cf6ad206644aec9176ae1e77bd
Matija Abicic <matija.abicic@gmail.com>
2016-02-24 21:23:17 -0800

    cleaning up server.js from copy-paste leftovers

--- server.js
+++ server.js
@@ -24,51 +24,11 @@ app.use(ua.middleware(settings.GA));
 app.get('/api', function(req, res){
   //google pageview tracking
   visitor.pageview("/api").send();
-  res.send('Next Chelsea match is ' + (global.nextMatch || 'not scheduled') + '.');
+  res.send('Api request received...');
 });
 
-//early slack api. only knows how to respond with the time left until the next match
-app.get('/api/slack', function(req, res){
-  var originHost = req.headers.host;
 
-  //exclude local api requests from Google Analytics tracking
-  if(settings.gaIgnoreHosts.indexOf(req.header.host)!=-1){
-    console.log('tracking request in analytics.');
-    visitor.pageview("/api/slack").send();
-  }
-
-  //construc the response
-  var jsonResponse = {};
-  jsonResponse.text = 'Next Chelsea match is ' + (global.nextMatch || 'not scheduled' + '.');
-  jsonResponse.nextMatchDate = global.nextMatch;
-  jsonResponse.nextOpponent = global.nextOpponent;
-  jsonResponse.lastPhrase = global.lastPhrase;
-  jsonResponse.lastServerStart = global.lastServerStart;
-  //console.log(req);
-  res.send(jsonResponse);
-});
-
-//let's define options for our recurrent http request
-var options = {
-  url: tokens.resultsEndpoint,
-  headers: {
-    'X-Auth-Token' : process.env.resultsToken
-  }
-};
-
-
+//start the server and listen on the designated port
 var server = app.listen(process.env.PORT || PORT, function(){
   console.log("Server started at localhost:%s", PORT);
-  global.lastServerStart = new Date();
-
-  //keep a track of last matchday commented, to avoid duplicates.
-  //this needs refactoring
-  global.lastPreMatchComment = null;
-  global.lastPostMatchComment = null;
-
-  //set up a timer.
-  setInterval(function(){
-    console.log("Tick...next match is " + (nextMatch || "not scheduled."));
-    request(options, callback.callback);
-  }, settings.pingInteralInMilliseconds);
 });
