dc1b59548a2c4d349890def9f86463970b2c8571
Matija Abicic <matija.abicic@gmail.com>
2016-02-26 23:30:26 -0800

    modularize callback

--- callback.js
+++ callback.js
@@ -3,11 +3,12 @@
 var callback = function(error, response, body){
   if (!error && response.statusCode==200){
 
+    console.log(body);
+    global.urban_response = body;
 
   }
   else {
-    console.log(error);
-    return error;
+    console.error();
   }
 };
 
