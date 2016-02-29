6fd8dbf2658f0e35a4f6ce9dd6ac5acbe43a8100
YouCantTriforce <michal.wilski@gmail.com>
2016-02-29 20:17:17 +0100

    - fixed returning of message when term is not present in the dictionary - restructured flow

--- urban-parser.js
+++ urban-parser.js
@@ -8,21 +8,42 @@ module.exports.parse = function (bodyString){
     if(bodyString){
       //return bodyString.list[0].definition;
       var data = JSON.parse(bodyString);
-      var numberOfDefinitions = data.list.length;
-      if (numberOfDefinitions > 0)
+
+      switch(data.result_type)
       {
-        var definition = data.list[0].definition;
-        var example = data.list[0].example;
+        case "exact":
+        {
+          var numberOfDefinitions = data.list.length;
+          if (numberOfDefinitions > 0)
+          {
+            var definition = data.list[0].definition;
+            var example = data.list[0].example;
 
-        //returnData.numberOfDefinitions = numberOfDefinitions;
-        returnData.text = '*DEFINITION (1 of ' + numberOfDefinitions +'):* ' + definition;
-        returnData.attachments = [{"title":"Example use", "text": example}];
-        return returnData;
+            returnData.text = '*DEFINITION (1 of ' + numberOfDefinitions +'):* ' + definition;
+            returnData.attachments = [{"title":"Example use", "text": example}];
+          }
+          else
+          {
+              returnData.text = 'No definition.';
+          }
+        }
+        break;
+
+        case "no_results":
+        {
+          returnData.text = 'No definition.';
+        }
+
+        default:
+        {
+          returnData.text = "No definition.";  // throw "Unexpected behaviour.";
+        }
       }
-
     }
-    //if there is no body, jsut return "no definition".
-    else returnData.text = 'No definition.';
+    else //service should return response even if there are no entries so I conclude there is something wrong with it
+    {
+      returnData.text = 'Dictionary service unavailable.';
+    }
 
     //return slack response.
     return returnData;
