4a921f1db2a2725ca3d01c65d1ada48983c54a4a
YouCantTriforce <michal.wilski@gmail.com>
2016-03-03 20:49:17 +0100

    draft of mocha testing

---
+++ mocha_testing/tests.js
@@ -0,0 +1,19 @@
+
+var assert = require("assert");
+var parser = require("../urban-parser.js");
+var fs = require("fs");
+
+var obj = JSON.parse(fs.readFileSync('mocha_testing/sampledata.js', 'utf8'));
+
+describe("Internal testing", function() {
+  describe("urban-parser", function() {
+    it("parses correct json result", function() {
+      var api_result = JSON.stringify(obj.urbanparser.apiresponse);
+
+      var expected_output = JSON.stringify(obj.urbanparser.correctoutput);
+      var parse_result = JSON.stringify(parser.parse(api_result, 'ephemeral'));
+
+      assert.equal(parse_result, expected_output);
+    });
+  });
+});
