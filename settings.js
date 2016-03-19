fe14988080bfa7e16eb4139fd850a5f77a8ef302
Matija Abicic <matija.abicic@gmail.com>
2016-03-18 22:25:37 -0700

    #22 - SFW feature added

--- settings.js
+++ settings.js
@@ -11,9 +11,13 @@ module.exports = {
   // ephemeral - only visible to user who isseus the slash command
   // in_channel - visible to everyone in the chat window (direct, group, channel)
   defaultSlackResponseMode  : 'ephemeral',
+
   slackOAuthURI             : 'https://slack.com/api/oauth.access',
   slackRedirectURI_local    : 'http://localhost:5000/AddSlack',
   slackRedirectURI_heroku   : 'https://urban-slack.herokuapp.com/AddSlack',
-  mongoDBName               : 'urbanslack'
+  mongoDBName               : 'urbanslack',
+
+  //by default, do not filter out profanity. 
+  defaultRating             : 'nsfw'
 
 };
