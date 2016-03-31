1ecc30874a3ce1416eea912b767b8fa3e1413800
Matija Abicic <matija.abicic@gmail.com>
2016-03-31 16:30:52 -0700

    maintenance

---
+++ secrets.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret_slack_client_ID      : "=====  Your Slack App Client ID  =====",
+  secret_slack_client_secret  : "=====  Your Slack App Secret     =====",
+  mLabApiKey                  : "=====  Your mLab API key         ====="
+};
