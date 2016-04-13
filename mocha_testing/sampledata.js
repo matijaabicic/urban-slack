4194e6e64468686d7180aa08a61005d55300a2a8
Matija Abicic <matija.abicic@gmail.com>
2016-04-12 22:47:28 -0700

    #39 and other tests

--- mocha_testing/sampledata.js
+++ mocha_testing/sampledata.js
@@ -9,7 +9,7 @@
         "word": "asdasdasd",
         "author": "etymologynerd1950",
         "permalink": "http://asdasdasd.urbanup.com/6394950",
-        "definition": "left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
+        "definition": "left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar dick to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
         "example": "asdasdasdasdsdasdasdadasdsd",
         "thumbs_up": 187,
         "thumbs_down": 65,
@@ -21,7 +21,25 @@
     "correctoutput": {
       "response_type": "ephemeral",
       "username": "UrbanSlack",
-      "text": "*DEFINITION (1 of 1):* left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
+      "text": "*DEFINITION (1 of 1):* left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar dick to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
+      "attachments": [{
+        "title": "Example use",
+        "text": "asdasdasdasdsdasdasdadasdsd"
+      }]
+    },
+    "sfwOutput":{
+      "response_type": "ephemeral",
+      "username": "UrbanSlack",
+      "text": "*DEFINITION (1 of 1):* left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar **** to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
+      "attachments": [{
+        "title": "Example use",
+        "text": "asdasdasdasdsdasdasdadasdsd"
+      }]
+    },
+    "publicOutput":{
+      "response_type": "in_channel",
+      "username": "UrbanSlack",
+      "text": "*DEFINITION (1 of 1):* left handed three finger pinkie to middle typing boredom filler like tapping those fingers on a table, similar **** to\n\nal;sdkjf;aslkdjf;alskdjf;alskdjf;alsdkjf, the alternating eight finger pinky to forefinger right and left hand keyboard doodling",
       "attachments": [{
         "title": "Example use",
         "text": "asdasdasdasdsdasdasdadasdsd"
--- mocha_testing/tests.js
+++ mocha_testing/tests.js
@@ -7,12 +7,42 @@ var obj = JSON.parse(fs.readFileSync('mocha_testing/sampledata.js', 'utf8'));
 
 describe("Internal testing", function() {
   describe("urban-parser", function() {
-    it("parses correct json result", function() {
+    it("Parses simple, private response", function() {
       var api_result = JSON.stringify(obj.urbanparser.apiresponse);
 
       var expected_output = JSON.stringify(obj.urbanparser.correctoutput);
-      var parse_result = JSON.stringify(parser.parse(api_result, JSON.parse('{"responseType":"ephemeral","rating":"nsfw","random":false}')));
+      var parse_result = JSON.stringify(parser.parse(api_result, JSON.parse('{"responseType":"ephemeral"}')));
 
+      assert.equal(parse_result, expected_output);
+    });
+    it("Censors NSFW content", function (){
+      //test data
+      var api_result = JSON.stringify(obj.urbanparser.apiresponse);
+      //expected data
+      var expected_output = JSON.stringify(obj.urbanparser.sfwOutput);
+      //actual result
+      var parse_result = JSON.stringify(parser.parse(api_result, JSON.parse('{"responseType":"ephemeral","rating":"sfw","random":false}')));
+      //compare and assert test
+      assert.equal(parse_result, expected_output);
+    });
+    it("Responds with NSFW content", function (){
+      //test data
+      var api_result = JSON.stringify(obj.urbanparser.apiresponse);
+      //expected data
+      var expected_output = JSON.stringify(obj.urbanparser.correctoutput);
+      //actual result
+      var parse_result = JSON.stringify(parser.parse(api_result, JSON.parse('{"responseType":"ephemeral","rating":"nsfw","random":false}')));
+      //compare and assert test
+      assert.equal(parse_result, expected_output);
+    });
+    it("Responds publically in channel", function (){
+      //test data
+      var api_result = JSON.stringify(obj.urbanparser.apiresponse);
+      //expected data
+      var expected_output = JSON.stringify(obj.urbanparser.publicOutput);
+      //actual result
+      var parse_result = JSON.stringify(parser.parse(api_result, JSON.parse('{"responseType":"in_channel","rating":"sfw","random":false}')));
+      //compare and assert test
       assert.equal(parse_result, expected_output);
     });
   });
