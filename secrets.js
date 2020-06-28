434fd9ca09f9d5028968670b7b4f9fa240b53811
Matija Abicic <matija.abicic@gmail.com>
2020-06-28 09:49:03 -0700

    #51 - Update scopes and fix EJS parser issues

--- secrets.js
+++ secrets.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secret_slack_client_ID      : "=====  Your Slack App Client ID  =====",
-  secret_slack_client_secret  : "=====  Your Slack App Secret     =====",
-  mLabApiKey                  : "=====  Your mLab API key         ====="
+  secret_slack_client_ID      : "13455138151.25936209731",
+  secret_slack_client_secret  : "3ee871c03671b3a402683e0302692fa5",
+  mLabApiKey                  : "aNodXH4adiiyJQ3Y8JEThoi-VOOc0Y7K"
 };
